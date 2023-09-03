b6e08fb7c837861463c1dd7bca91b39b24fe694f
leenalovisav <l.viigisalu@voco.ee>
2023-09-04 01:16:17 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express, { Express, Request, Response } from "express";
+import cors from 'cors';
+
+const app: Express = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(3000,() => {
+  console.log(`[server]: Server is running at http://localhost:3000`);
+});
